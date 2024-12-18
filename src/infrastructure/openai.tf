9cea4ac0c94fedb5eb23ae20092586947229e40b
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-18 22:56:54 +0100

    open ai

--- src/infrastructure/openai.tf
+++ src/infrastructure/openai.tf
@@ -1,7 +1,18 @@
-resource "azurerm_cognitive_account" "cognitive_service" {
-  name                = "ai-assistant-openai-account-${var.environment_prefix}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "S0"
-  kind                = "CognitiveServices"
+module "openai" {
+  source                        = "Azure/openai/azurerm"
+  version                       = "0.1.5"
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  account_name                  = "open-ai-account-${var.environment_prefix}"
+  sku_name                      = "S0"
+  public_network_access_enabled = true
+  deployment = {
+    "gpt-4-turbo" = {
+      name         = "gpt-4-turbo"
+      model_format = "OpenAI"
+      model_name   = "gpt-4o"
+      scale_type   = "Standard"
+      capacity     = 1
+    }
+  }
 }
