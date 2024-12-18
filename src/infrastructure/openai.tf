711938bef9661b9c519cd2cf7f4a5f2bab397528
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-19 00:49:50 +0100

    sequential run

--- src/infrastructure/openai.tf
+++
@@ -1,6 +0,0 @@
-module "openai" {
-  source              = "./openai"
-  location            = var.location
-  account_name        = "open-ai-services-account-${var.environment_prefix}"
-  resource_group_name = azurerm_resource_group.rg.name
-}
