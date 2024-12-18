02b7810acb411c455974c49b6c417389ed84aae9
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-18 22:17:45 +0100

    fix names

--- src/infrastructure/functionapp.tf
+++ src/infrastructure/functionapp.tf
@@ -1,29 +1,25 @@
 resource "azurerm_storage_account" "function_storage" {
-  name                     = "safunctionapp${var.environment_prefix}"
+  name                     = "saaifunctionapp${var.environment_prefix}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags = {
-    environment = var.environment_name
-  }
+  tags                     = local.tags
 }
 
 resource "azurerm_service_plan" "function_sp" {
-  name                = "sp_ai_assistant_function${var.environment_prefix}"
+  name                = "sp-ai-assistant-function-${var.environment_prefix}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
   os_type  = "Windows"
   sku_name = "Y1"
 
-  tags = {
-    environment = var.environment_name
-  }
+  tags = local.tags
 }
 
 resource "azurerm_function_app" "function_app" {
-  name                       = "test-azure-functions"
+  name                       = "fn-ai-assistant-function-${var.environment_prefix}"
   location                   = var.location
   resource_group_name        = azurerm_resource_group.rg.name
   app_service_plan_id        = azurerm_service_plan.function_sp.id
--- src/infrastructure/locals.tf
+++ src/infrastructure/locals.tf
@@ -1,3 +1,7 @@
 locals {
-
+  appName = "ai-antispam-bot"
+  tags = {
+    environment = var.environment_name
+    appName     = local.appName
+  }
 }
