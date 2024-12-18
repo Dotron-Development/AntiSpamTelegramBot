503a582dc2d7288a2c5d0feb2f2dcf26728423df
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-18 22:22:29 +0100

    fix name

--- src/infrastructure/functionapp.tf
+++ src/infrastructure/functionapp.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "function_storage" {
-  name                     = "saaifunctionapp${var.environment_prefix}"
+  name                     = "saaiassistantfnapp${var.environment_prefix}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
