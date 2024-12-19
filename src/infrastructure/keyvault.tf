a6a6dc6d478a5fdd9de3f9450da8362c6ec1f0b0
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-20 00:36:34 +0100

    no message

--- src/infrastructure/keyvault.tf
+++ src/infrastructure/keyvault.tf
@@ -62,6 +62,16 @@ resource "azurerm_key_vault" "kv" {
 
     secret_permissions = ["Get"]
   }
+
+  ## access for terraform to manage the key vault
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions     = ["Get", "List"]
+    secret_permissions  = ["Get", "List"]
+    storage_permissions = ["Get"]
+  }
 }
 
 
