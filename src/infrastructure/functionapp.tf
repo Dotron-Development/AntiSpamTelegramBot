6dffb4efe317d84b43164130f6746cf8428bb7fb
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-20 00:30:06 +0100

    no message

--- src/infrastructure/functionapp.tf
+++ src/infrastructure/functionapp.tf
@@ -19,19 +19,20 @@ resource "azurerm_service_plan" "function_sp" {
 }
 
 resource "azurerm_linux_function_app" "function_app" {
-  name                       = "fn-ai-assistant-function-${var.environment_prefix}"
-  location                   = var.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  service_plan_id            = azurerm_service_plan.function_sp.id
-  storage_account_name       = azurerm_storage_account.function_storage.name
-  storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
+  name                            = "fn-ai-assistant-function-${var.environment_prefix}"
+  location                        = var.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  service_plan_id                 = azurerm_service_plan.function_sp.id
+  storage_account_name            = azurerm_storage_account.function_storage.name
+  storage_account_access_key      = azurerm_storage_account.function_storage.primary_access_key
+  key_vault_reference_identity_id = azurerm_user_assigned_identity.functionapp_identity.principal_id
 
   site_config {
     always_on = false
   }
 
   app_settings = {
-    "AzureWebJobsStorage"         = azurerm_storage_account.storage.primary_connection_string
+    "AzureWebJobsStorage"         = azurerm_storage_account.function_storage.primary_connection_string
     "FUNCTIONS_EXTENSION_VERSION" = "~4"
     "KEY_VAULT_SECRET"            = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_token.id})"
   }
