ee25aad31919331edc9c2ba4ec1dc70b96a62d10
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-19 21:23:29 +0100

    no message

--- src/infrastructure/openai/main.tf
+++ src/infrastructure/openai/main.tf
@@ -4,6 +4,6 @@ data "terraform_remote_state" "data" {
     resource_group_name  = var.main_state_resource_group_name
     storage_account_name = var.main_state_storage_account_name
     container_name       = var.main_state_container_name
-    key                  = var.main_key
+    key                  = var.main_state_key
   }
 }
