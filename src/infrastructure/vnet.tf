a76eea4295f3980fe5c8fe7d1389beba858522a3
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-20 19:00:34 +0100

    no message

--- src/infrastructure/vnet.tf
+++ src/infrastructure/vnet.tf
@@ -6,7 +6,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet_for_vm" {
-  name                 = "vnet-vm-runners-${var.environment_prefix}"
+  name                 = "subnet-vm-runners-${var.environment_prefix}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
