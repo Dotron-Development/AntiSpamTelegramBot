3f5ef1ebf2c50b6804094df667b1db81d90914eb
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-31 00:13:12 +0100

    no message

--- src/infrastructure/appinsights.tf
+++ src/infrastructure/appinsights.tf
@@ -7,7 +7,7 @@ resource "azurerm_log_analytics_workspace" "analytics_workspace" {
 }
 
 resource "azurerm_application_insights" "appinsights" {
-  name                = "ai-${local.appName}-${var.environment_prefix}"
+  name                = "app-insights-${local.appName}-${var.environment_prefix}"
   location            = var.location
   resource_group_name = data.terraform_remote_state.openai_data.outputs.resource_group_name
   workspace_id        = azurerm_log_analytics_workspace.analytics_workspace.id
