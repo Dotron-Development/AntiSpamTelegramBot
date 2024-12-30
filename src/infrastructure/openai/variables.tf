5a4de7fdd3e914e2c21af310858759666d5fe75d
Serhii Trofimov <trofimov.developer@gmail.com>
2024-12-30 21:55:54 +0100

    no message

--- src/infrastructure/openai/variables.tf
+++ src/infrastructure/openai/variables.tf
@@ -12,23 +12,3 @@ variable "environment_name" {
   description = "The environment name."
   type        = string
 }
-
-variable "openai_state_resource_group_name" {
-  description = "The name of the terraform backend resource group name."
-  type        = string
-}
-
-variable "openai_state_storage_account_name" {
-  description = "The name of the terraform backend storage account."
-  type        = string
-}
-
-variable "openai_state_container_name" {
-  description = "The name of the terraform backend container."
-  type        = string
-}
-
-variable "openai_state_key" {
-  description = "The main state file key (path)."
-  type        = string
-}
