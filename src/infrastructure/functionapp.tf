1d9ad3cd52ef162994eec19c116db21f801f2147
Serhii Trofimov <trofimov.developer@gmail.com>
2025-01-02 01:28:37 +0100

    no message

--- src/infrastructure/functionapp.tf
+++ src/infrastructure/functionapp.tf
@@ -47,8 +47,6 @@ resource "azurerm_windows_function_app" "function_app" {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
     "FUNCTIONS_WORKER_RUNTIME" = "dotnet-isolated"
 
-    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.appinsights.connection_string
-
     "OpenAiServicesConfiguration__ImageRecognitionDeployment" = module.global_constants.image_text_extraction_model_name
     "OpenAiServicesConfiguration__SpamRecognitionDeployment"  = module.global_constants.spam_recognition_model_name
     "OpenAiServicesConfiguration__ServiceUrl"                 = data.terraform_remote_state.openai_data.outputs.openai_service_url
