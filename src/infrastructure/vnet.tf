042c55d12c267a71c0912712f37424a5111d9d17
Serhii Trofimov <trofimov.developer@gmail.com>
2025-01-09 23:26:45 +0100

    no message

--- src/infrastructure/vnet.tf
+++ src/infrastructure/vnet.tf
@@ -12,7 +12,8 @@ resource "azurerm_subnet" "subnet1" {
   resource_group_name  = data.terraform_remote_state.openai_data.outputs.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-  # if public access is disabled, we need to create a service endpoint
-  # connectivity will be done through the private endpoint
-  service_endpoints = var.disable_public_access ? ["Microsoft.KeyVault"] : []
+
+  # if public access is enabled, we need to create a service endpoint
+  # if public access is disabled then connectivity will be done through the private endpoint
+  service_endpoints = !var.disable_public_access ? ["Microsoft.KeyVault"] : []
 }
