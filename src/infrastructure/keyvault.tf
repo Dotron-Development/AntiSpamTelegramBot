306898a2fdbe11fae39b93ecf2eae780b3a7db8e
Serhii Trofimov <trofimov.developer@gmail.com>
2025-06-26 03:39:35 +0200

    access to KV from github runner

--- src/infrastructure/keyvault.tf
+++ src/infrastructure/keyvault.tf
@@ -15,6 +15,13 @@ resource "azurerm_key_vault" "kv" {
   network_acls {
     bypass         = "AzureServices"
     default_action = "Deny"
+
+    # access from github runner subnet to key vault
+    # only if public access is enabled
+    virtual_network_subnet_ids = !var.disable_public_access ? [
+      azurerm_subnet.subnet1.id,
+      data.azurerm_subnet.github_runner_vnet_subnet.id
+    ] : []
   }
 
   tags = local.tags
