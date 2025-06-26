d2b7d0052fd9aebdaaf0203225a7a9b664074c7e
Serhii Trofimov <trofimov.developer@gmail.com>
2025-06-26 18:00:07 +0200

    specify the identity which is used for KV reference

--- src/infrastructure/outputs.tf
+++ src/infrastructure/outputs.tf
@@ -6,3 +6,6 @@ output "functionapp_name" {
   value = azurerm_function_app_flex_consumption.function_app.name
 }
 
+output "function_app_identity_id" {
+  value = azurerm_user_assigned_identity.functionapp_identity.principal_id
+}
