988839d5d78639bf906b6261700530b43d9a07f2
Serhii Trofimov <trofimov.developer@gmail.com>
2025-06-28 00:39:07 +0200

    remove temp fn

--- src/infrastructure/functionapp.tf
+++ src/infrastructure/functionapp.tf
@@ -81,47 +81,47 @@ resource "azurerm_service_plan" "temp_sp" {
   tags = local.tags
 }
 
-resource "azurerm_windows_function_app" "temp" {
-  name                       = "fn-temp-${local.appName}-${var.environment_prefix}"
-  location                   = var.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  service_plan_id            = azurerm_service_plan.temp_sp.id
-  storage_account_name       = azurerm_storage_account.function_storage.name
-  storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
-  https_only                 = true
+# resource "azurerm_windows_function_app" "temp" {
+#   name                       = "fn-temp-${local.appName}-${var.environment_prefix}"
+#   location                   = var.location
+#   resource_group_name        = azurerm_resource_group.rg.name
+#   service_plan_id            = azurerm_service_plan.temp_sp.id
+#   storage_account_name       = azurerm_storage_account.function_storage.name
+#   storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
+#   https_only                 = true
 
-  site_config {
-    always_on                              = false
-    app_scale_limit                        = 5
-    use_32_bit_worker                      = false
-    application_insights_connection_string = azurerm_application_insights.appinsights.connection_string
-    application_insights_key               = azurerm_application_insights.appinsights.instrumentation_key
+#   site_config {
+#     always_on                              = false
+#     app_scale_limit                        = 5
+#     use_32_bit_worker                      = false
+#     application_insights_connection_string = azurerm_application_insights.appinsights.connection_string
+#     application_insights_key               = azurerm_application_insights.appinsights.instrumentation_key
 
-    application_stack {
-      dotnet_version              = "v9.0"
-      use_dotnet_isolated_runtime = true
-    }
-  }
+#     application_stack {
+#       dotnet_version              = "v9.0"
+#       use_dotnet_isolated_runtime = true
+#     }
+#   }
 
-  app_settings = {
+#   app_settings = {
 
-    "OpenAiServicesConfiguration__ImageRecognitionDeployment" = module.global_constants.image_text_extraction_model_name
-    "OpenAiServicesConfiguration__SpamRecognitionDeployment"  = module.global_constants.spam_recognition_model_name
-    "OpenAiServicesConfiguration__ServiceUrl"                 = module.avm-res-cognitiveservices-account.endpoint
+#     "OpenAiServicesConfiguration__ImageRecognitionDeployment" = module.global_constants.image_text_extraction_model_name
+#     "OpenAiServicesConfiguration__SpamRecognitionDeployment"  = module.global_constants.spam_recognition_model_name
+#     "OpenAiServicesConfiguration__ServiceUrl"                 = module.avm-res-cognitiveservices-account.endpoint
 
-    "AzureTablesConfiguration__StorageAccountUrl" = azurerm_storage_account.main_storage.primary_web_endpoint
+#     "AzureTablesConfiguration__StorageAccountUrl" = azurerm_storage_account.main_storage.primary_web_endpoint
 
-    "TelegramBotConfiguration__DebugAiResponse"     = "false"
-    "TelegramBotConfiguration__ForwardSpamToChatId" = "-1002395980780"
-    "TelegramBotConfiguration__BotName"             = var.botName,
-    "TelegramBotConfiguration__SecretHeader"        = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_secret_header.id})"
-    "TelegramBotConfiguration__Token"               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_token.id})"
-  }
+#     "TelegramBotConfiguration__DebugAiResponse"     = "false"
+#     "TelegramBotConfiguration__ForwardSpamToChatId" = "-1002395980780"
+#     "TelegramBotConfiguration__BotName"             = var.botName,
+#     "TelegramBotConfiguration__SecretHeader"        = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_secret_header.id})"
+#     "TelegramBotConfiguration__Token"               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_token.id})"
+#   }
 
-  identity {
-    type = "UserAssigned"
-    identity_ids = [
-      azurerm_user_assigned_identity.functionapp_identity.id
-    ]
-  }
-}
+#   identity {
+#     type = "UserAssigned"
+#     identity_ids = [
+#       azurerm_user_assigned_identity.functionapp_identity.id
+#     ]
+#   }
+# }
