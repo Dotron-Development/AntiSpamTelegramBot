7a96859c2edf9c2ba2e996e6b734e22d7c394ba2
Serhii Trofimov <trofimov.developer@gmail.com>
2025-06-29 13:20:07 +0200

    fix vnet index

--- src/infrastructure/keyvault.tf
+++ src/infrastructure/keyvault.tf
@@ -36,7 +36,7 @@ resource "azurerm_private_dns_zone" "private_dns" {
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault_vnet_link" {
   count                 = var.disable_public_access ? 1 : 0
   name                  = "vnl-${local.kv_name}-${var.environment_prefix}"
-  virtual_network_id    = azurerm_virtual_network.vnet[0].id
+  virtual_network_id    = azurerm_virtual_network.vnet.id
   private_dns_zone_name = azurerm_private_dns_zone.private_dns[0].name
   resource_group_name   = azurerm_resource_group.rg.name
   tags                  = local.tags
--- src/infrastructure/vnet.tf
+++ src/infrastructure/vnet.tf
@@ -10,6 +10,6 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1-${var.environment_prefix}"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet[0].name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
