1b47f5e17994ac32c6e5955c63d9b6d4c9b22796
Serhii Trofimov <trofimov.developer@gmail.com>
2025-06-29 02:52:35 +0200

    recover all

--- src/infrastructure/storage.tf
+++ src/infrastructure/storage.tf
@@ -1,28 +1,28 @@
-# resource "azurerm_storage_account" "data_storage" {
-#   name                     = "satgbotdata${var.environment_prefix}"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-#   tags                     = local.tags
-# }
+resource "azurerm_storage_account" "data_storage" {
+  name                     = "satgbotdata${var.environment_prefix}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  tags                     = local.tags
+}
 
-# resource "azurerm_storage_table" "spam_stats" {
-#   name                 = "SpamStats"
-#   storage_account_name = azurerm_storage_account.data_storage.name
-# }
+resource "azurerm_storage_table" "spam_stats" {
+  name                 = "SpamStats"
+  storage_account_name = azurerm_storage_account.data_storage.name
+}
 
-# resource "azurerm_storage_table" "spam_history" {
-#   name                 = "SpamHistory"
-#   storage_account_name = azurerm_storage_account.data_storage.name
-# }
+resource "azurerm_storage_table" "spam_history" {
+  name                 = "SpamHistory"
+  storage_account_name = azurerm_storage_account.data_storage.name
+}
 
-# resource "azurerm_storage_table" "spam_hash" {
-#   name                 = "SpamHash"
-#   storage_account_name = azurerm_storage_account.data_storage.name
-# }
+resource "azurerm_storage_table" "spam_hash" {
+  name                 = "SpamHash"
+  storage_account_name = azurerm_storage_account.data_storage.name
+}
 
-# resource "azurerm_storage_table" "message_count" {
-#   name                 = "MessageCount"
-#   storage_account_name = azurerm_storage_account.data_storage.name
-# }
+resource "azurerm_storage_table" "message_count" {
+  name                 = "MessageCount"
+  storage_account_name = azurerm_storage_account.data_storage.name
+}
