ed2c7ac7750f5258f0fc1f54b15f1a1eb37ba8f1
Serhii Trofimov <trofimov.developer@gmail.com>
2025-06-29 02:47:04 +0200

    destroy some resourses

--- src/infrastructure/functionapp.tf
+++ src/infrastructure/functionapp.tf
@@ -1,72 +1,72 @@
-locals {
-  function_app_name = "fn-${local.appName}-${var.environment_prefix}"
-}
-resource "azurerm_storage_account" "function_storage" {
-  name                     = "satgbotfnapp${var.environment_prefix}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags                     = local.tags
-}
+# locals {
+#   function_app_name = "fn-${local.appName}-${var.environment_prefix}"
+# }
+# resource "azurerm_storage_account" "function_storage" {
+#   name                     = "satgbotfnapp${var.environment_prefix}"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = var.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   tags                     = local.tags
+# }
 
-resource "azurerm_storage_container" "function_storage_container" {
-  name                  = "${local.function_app_name}-flexcontainer"
-  storage_account_id    = azurerm_storage_account.function_storage.id
-  container_access_type = "private"
-}
+# resource "azurerm_storage_container" "function_storage_container" {
+#   name                  = "${local.function_app_name}-flexcontainer"
+#   storage_account_id    = azurerm_storage_account.function_storage.id
+#   container_access_type = "private"
+# }
 
-resource "azurerm_service_plan" "function_sp" {
-  name                = "sp-${local.appName}-fn-${var.environment_prefix}"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_service_plan" "function_sp" {
+#   name                = "sp-${local.appName}-fn-${var.environment_prefix}"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  os_type  = "Linux"
-  sku_name = "FC1"
+#   os_type  = "Linux"
+#   sku_name = "FC1"
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "azurerm_function_app_flex_consumption" "function_app" {
-  name                = local.function_app_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.function_sp.id
+# resource "azurerm_function_app_flex_consumption" "function_app" {
+#   name                = local.function_app_name
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   service_plan_id     = azurerm_service_plan.function_sp.id
 
-  storage_container_type            = "blobContainer"
-  storage_container_endpoint        = "${azurerm_storage_account.function_storage.primary_blob_endpoint}${azurerm_storage_container.function_storage_container.name}"
-  storage_authentication_type       = "UserAssignedIdentity"
-  storage_user_assigned_identity_id = azurerm_user_assigned_identity.functionapp_identity.id
-  runtime_name                      = "dotnet-isolated"
-  runtime_version                   = "9.0"
-  instance_memory_in_mb             = 2048
+#   storage_container_type            = "blobContainer"
+#   storage_container_endpoint        = "${azurerm_storage_account.function_storage.primary_blob_endpoint}${azurerm_storage_container.function_storage_container.name}"
+#   storage_authentication_type       = "UserAssignedIdentity"
+#   storage_user_assigned_identity_id = azurerm_user_assigned_identity.functionapp_identity.id
+#   runtime_name                      = "dotnet-isolated"
+#   runtime_version                   = "9.0"
+#   instance_memory_in_mb             = 2048
 
-  site_config {
-    application_insights_connection_string = azurerm_application_insights.appinsights.connection_string
-    application_insights_key               = azurerm_application_insights.appinsights.instrumentation_key
-  }
+#   site_config {
+#     application_insights_connection_string = azurerm_application_insights.appinsights.connection_string
+#     application_insights_key               = azurerm_application_insights.appinsights.instrumentation_key
+#   }
 
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.functionapp_identity.id]
-  }
+#   identity {
+#     type         = "UserAssigned"
+#     identity_ids = [azurerm_user_assigned_identity.functionapp_identity.id]
+#   }
 
-  app_settings = merge({
-    "OpenAiServicesConfiguration__ImageRecognitionDeployment" = module.global_constants.image_text_extraction_model_name
-    "OpenAiServicesConfiguration__SpamRecognitionDeployment"  = module.global_constants.spam_recognition_model_name
-    "OpenAiServicesConfiguration__ServiceUrl"                 = module.avm-res-cognitiveservices-account.endpoint
-    "OpenAiServicesConfiguration__OpenAiIdentityClientId"     = azurerm_user_assigned_identity.functionapp_identity.client_id
+#   app_settings = merge({
+#     "OpenAiServicesConfiguration__ImageRecognitionDeployment" = module.global_constants.image_text_extraction_model_name
+#     "OpenAiServicesConfiguration__SpamRecognitionDeployment"  = module.global_constants.spam_recognition_model_name
+#     "OpenAiServicesConfiguration__ServiceUrl"                 = module.avm-res-cognitiveservices-account.endpoint
+#     "OpenAiServicesConfiguration__OpenAiIdentityClientId"     = azurerm_user_assigned_identity.functionapp_identity.client_id
 
-    "AzureTablesConfiguration__StorageAccountUrl"     = azurerm_storage_account.data_storage.primary_web_endpoint
-    "AzureTablesConfiguration__TableIdentityClientId" = azurerm_user_assigned_identity.functionapp_identity.client_id
+#     "AzureTablesConfiguration__StorageAccountUrl"     = azurerm_storage_account.data_storage.primary_web_endpoint
+#     "AzureTablesConfiguration__TableIdentityClientId" = azurerm_user_assigned_identity.functionapp_identity.client_id
 
-    "TelegramBotConfiguration__ForwardSpamToChatId" = var.forwardSpamToChatId,
-    "TelegramBotConfiguration__BotName"             = var.bot_name,
-    "TelegramBotConfiguration__SecretHeader"        = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_secret_header.id})"
-    "TelegramBotConfiguration__Token"               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_token.id})"
-    }, var.debug_ai_response != null ? {
-    "TelegramBotConfiguration__DebugAiResponse" = var.debug_ai_response
-  } : {})
+#     "TelegramBotConfiguration__ForwardSpamToChatId" = var.forwardSpamToChatId,
+#     "TelegramBotConfiguration__BotName"             = var.bot_name,
+#     "TelegramBotConfiguration__SecretHeader"        = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_secret_header.id})"
+#     "TelegramBotConfiguration__Token"               = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.telegram_bot_token.id})"
+#     }, var.debug_ai_response != null ? {
+#     "TelegramBotConfiguration__DebugAiResponse" = var.debug_ai_response
+#   } : {})
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
--- src/infrastructure/functionapp_user_identity.tf
+++ src/infrastructure/functionapp_user_identity.tf
@@ -1,27 +1,27 @@
-resource "azurerm_user_assigned_identity" "functionapp_identity" {
-  location            = var.location
-  name                = "ua-mi-${local.appName}-fn-${var.environment_prefix}"
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = local.tags
-}
+# resource "azurerm_user_assigned_identity" "functionapp_identity" {
+#   location            = var.location
+#   name                = "ua-mi-${local.appName}-fn-${var.environment_prefix}"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   tags                = local.tags
+# }
 
-## Grant the identity access to the data storage account
-resource "azurerm_role_assignment" "functionapp_identity_data_storage_access" {
-  scope                = azurerm_storage_account.data_storage.id
-  role_definition_name = "Storage Table Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.functionapp_identity.principal_id
-}
+# ## Grant the identity access to the data storage account
+# resource "azurerm_role_assignment" "functionapp_identity_data_storage_access" {
+#   scope                = azurerm_storage_account.data_storage.id
+#   role_definition_name = "Storage Table Data Contributor"
+#   principal_id         = azurerm_user_assigned_identity.functionapp_identity.principal_id
+# }
 
-## Grant Read access for the function app to the Open AI services in the resource group
-resource "azurerm_role_assignment" "function_app_to_openai" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Cognitive Services OpenAI User"
-  principal_id         = azurerm_user_assigned_identity.functionapp_identity.principal_id
-}
+# ## Grant Read access for the function app to the Open AI services in the resource group
+# resource "azurerm_role_assignment" "function_app_to_openai" {
+#   scope                = azurerm_resource_group.rg.id
+#   role_definition_name = "Cognitive Services OpenAI User"
+#   principal_id         = azurerm_user_assigned_identity.functionapp_identity.principal_id
+# }
 
-## Grant the identity access to the application strorage account
-resource "azurerm_role_assignment" "functionapp_identity_storage_access_app" {
-  scope                = azurerm_storage_account.function_storage.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.functionapp_identity.principal_id
-}
+# ## Grant the identity access to the application strorage account
+# resource "azurerm_role_assignment" "functionapp_identity_storage_access_app" {
+#   scope                = azurerm_storage_account.function_storage.id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = azurerm_user_assigned_identity.functionapp_identity.principal_id
+# }
